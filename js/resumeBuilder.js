681eca6ba8a50bcd4129b0040e9c5d0e5acada00
kkas <kentakikui@gmail.com>
2015-09-26 20:13:34 +0900

    Add console.log() my name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append(["Kenta Kikui"]);
+
+var awesomeThoughts = "I am Kenta Kikui and I am AWESOME!";
+console.log(awesomeThoughts);
