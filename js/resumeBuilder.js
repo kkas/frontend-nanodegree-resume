78051d90a0a0b9718694dd3c8971cdd38a89db4d
kkas <kentakikui@gmail.com>
2015-09-27 09:00:38 +0900

    Clean out resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-$("#main").append(["Kenta Kikui"]);
-
-var awesomeThoughts = "I am Kenta Kikui and I am AWESOME!";
-console.log(awesomeThoughts);
-
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-$("#main").append(funThoughts);
-
-var formattedName = HTMLheaderName.replace("%data%", "Kenta Kikui");
-var formattedRole = HTMLheaderRole.replace("%data%", "Front-end Web Developer");
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
 var work = {
   "jobs": [
     {
